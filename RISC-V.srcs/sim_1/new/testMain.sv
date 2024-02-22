fe0eb0dffbfc265ff889d013454ff106e37a5990
prushton2 <peterrushton418@gmail.com>
2024-02-21 22:00:36 -0500

    modified maintest

--- RISC-V.srcs/sim_1/new/testMain.sv
+++ RISC-V.srcs/sim_1/new/testMain.sv
@@ -1,28 +1,27 @@
 module testMain();
 
-	reg clk;
-	reg reset;
-	reg counter = 0;
-	reg [63:0] activeKeyStroke;
+        reg clk;
+        reg reset;
+        reg [63:0] counter = 0;
+        reg [63:0] activeKeyStroke;
 
-	reg [63:0] keystrokes [63:0];
+        reg [63:0] keystrokes [63:0];
 
-	main main1(clk, reset, activeKeyStroke);
+        main main1(clk, reset, activeKeyStroke);
 
-	initial begin
-		reset = 1; #50;
-		reset = 0; #50;
-		$readmemb("keystrokes.dat", keystrokes);
-	end
+        initial begin
+                $readmemh("keystrokes.mem", keystrokes);
+                reset = 1; #50;
+                reset = 0; #50;
+        end
 
-	always begin
-		activeKeyStroke <= keystrokes[counter];
+        always begin
+                activeKeyStroke <= keystrokes[counter/10];
+                // /10 gives enough time for cpu to process keyboard input
+                clk = 0; #50;
+                clk = 1; #50;
 
-		clk = 0; #50;
-		clk = 1; #50;
-		
-		counter = counter+1;
-	end
+                counter = counter+1;
+        end
 
 endmodule
-
